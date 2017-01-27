0e084f173fa81a2024243b65296390d7a91b7d87
yangsx <yangsx@189.cn>
2017-01-27 22:50:10 +0800

    try use ipython to pasre offline file

--- web-crawl/douban/douban/spiders/book.py
+++ web-crawl/douban/douban/spiders/book.py
@@ -7,11 +7,27 @@ class BookSpider(scrapy.Spider):
     allowed_domains = ["book.douban.com"]
     start_urls = ['http://book.douban.com/tag/编程']
 
+    def _save_offline(self, fn, data):
+        fd = open(fn, "w")
+        fd.write(data)
+        fd.close()
+
     def parse(self, response):
         bksel = response.xpath("//li[@class='subject-item']")
         print("Totally %d items found" %(len(bksel)))
+        self._save_offline("/tmp/my.html", str(response.body))
+
         for it in bksel:
+            print(type(it.xpath("div/@class")))
+            print(type(it.xpath("div/@class").extract()))
+
             print(it.xpath("div/@class").extract())
+            print(it.xpath("div[@class='info']/h2/a/text()").extract()[0].strip())
+            if it.xpath("div[@class='pub']/text()").extract() is None:
+                print("BAD, empty")
+            else:
+                print(it.xpath("div[@class='pub']/text()").extract())
+#print(it.xpath("div[@class='star_clearfix']/span[@class='rating_nums']/text()").extract())
 #print(h2)
 
         pass
