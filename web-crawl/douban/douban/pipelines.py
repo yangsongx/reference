3771a0a62fd9e3a04142133b361567b44259430d
yangsx <yangsx@189.cn>
2017-02-01 20:35:26 +0800

    next phase will invovle item

--- web-crawl/douban/douban/pipelines.py
+++ web-crawl/douban/douban/pipelines.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from scrapy.exceptions import DropItem
+
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
--- web-crawl/douban/douban/spiders/book.py
+++ web-crawl/douban/douban/spiders/book.py
@@ -27,6 +27,8 @@ class BookSpider(scrapy.Spider):
         print("Totally %d items found" %(len(bksel)))
 
         for it in bksel:
+            bitem = BookItem()
+
             title = it.xpath("div[@class='info']/h2/a/text()").extract()[0].strip()
             if it.xpath("div[@class='info']/h2/a/span/text()").extract_first() != None:
                 title += it.xpath("div[@class='info']/h2/a/span/text()").extract_first()
@@ -34,6 +36,12 @@ class BookSpider(scrapy.Spider):
             rating = it.re(r"\<span class=\"rating_nums\"\>?([\s\S]*?)\<\/span\>")[0]
             pubinfo = it.re(r"\<div class=\"pub\"\>?([\s\S]*?)\<\/div\>")[0].strip()
             print("Title:%s  Rating:%s  Pub:%s" %(title, rating, pubinfo))
+            bitem["tag"] = 0 #Programing
+            bitem["title"] = title
+            bitem["author"] = pubinfo 
+            bitem["rating"] = rating 
+
+            yield bitem
 
 
         pass
