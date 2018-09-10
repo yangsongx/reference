4b5eeeca72693f381ae5d30f055f3713d0b272f1
yangsx <yangsongxiang@shuzijiayuan.com>
2018-09-10 13:36:38 +0800

    finished the first Programming TAG book scrapying

--- web-crawl/douban/douban/spiders/book.py
+++ web-crawl/douban/douban/spiders/book.py
@@ -14,7 +14,12 @@ from douban.spiders.items import BookItem
 class BookSpider(scrapy.Spider):
     name = "book"
     allowed_domains = ["book.douban.com"]
-    start_urls = ['http://book.douban.com/tag/编程?start=400&type=T']
+    start_urls = ['http://book.douban.com/tag/编程?start=940&type=T',
+                  'http://book.douban.com/tag/编程?start=960&type=T',
+                  'http://book.douban.com/tag/编程?start=980&type=T',
+                  'http://book.douban.com/tag/编程?start=1000&type=T',
+                  'http://book.douban.com/tag/编程?start=1020&type=T',
+                  'http://book.douban.com/tag/编程?start=1040&type=T',]
 
 
     # Just a internal util
@@ -34,7 +39,17 @@ class BookSpider(scrapy.Spider):
             if it.xpath("div[@class='info']/h2/a/span/text()").extract_first() != None:
                 title += it.xpath("div[@class='info']/h2/a/span/text()").extract_first()
 
-            rating = it.re(r"\<span class=\"rating_nums\"\>?([\s\S]*?)\<\/span\>")[0]
+
+            rating_match  = it.re(r"\<span class=\"rating_nums\"\>?([\s\S]*?)\<\/span\>")
+            if rating_match != None and len(rating_match) > 0:
+                if rating_match[0] != ''  and rating_match[0] != ' ':
+                    rating = rating_match[0]
+                else:
+                    rating = 0.0
+            else:
+                print("=========@@@@@@ not YET @@@@=====")
+                rating = 0.0 # not be rated yet
+
             pubinfo = it.re(r"\<div class=\"pub\"\>?([\s\S]*?)\<\/div\>")[0].strip()
             # 2018-09-07 : try extract the cover images...
             cover_data = it.xpath("div[@class='pic']/a[@class='nbg']/img/@src").extract()[0]
