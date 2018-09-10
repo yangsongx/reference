bb46b50c50a704958f33c41a066618a1581c3e08
yangsx <yangsongxiang@shuzijiayuan.com>
2018-09-10 17:10:01 +0800

    can auto scrapy the web now

--- web-crawl/douban/douban/spiders/book.py
+++ web-crawl/douban/douban/spiders/book.py
@@ -14,12 +14,18 @@ from douban.spiders.items import BookItem
 class BookSpider(scrapy.Spider):
     name = "book"
     allowed_domains = ["book.douban.com"]
-    start_urls = ['http://book.douban.com/tag/编程?start=940&type=T',
-                  'http://book.douban.com/tag/编程?start=960&type=T',
-                  'http://book.douban.com/tag/编程?start=980&type=T',
-                  'http://book.douban.com/tag/编程?start=1000&type=T',
-                  'http://book.douban.com/tag/编程?start=1020&type=T',
-                  'http://book.douban.com/tag/编程?start=1040&type=T',]
+
+    global_count = 0 # protection
+    leading_url = 'http://book.douban.com'
+
+    start_urls = ['http://book.douban.com/tag/儿童文学?start=0&type=T'] # only one starting...
+
+#    start_urls = ['http://book.douban.com/tag/编程?start=940&type=T',
+#                  'http://book.douban.com/tag/编程?start=960&type=T',
+#                  'http://book.douban.com/tag/编程?start=980&type=T',
+#                  'http://book.douban.com/tag/编程?start=1000&type=T',
+#                  'http://book.douban.com/tag/编程?start=1020&type=T',
+#                  'http://book.douban.com/tag/编程?start=1040&type=T',]
 
 
     # Just a internal util
@@ -28,13 +34,24 @@ class BookSpider(scrapy.Spider):
         fd.write(data)
         fd.close()
 
+#    def start_requests(self):
+#
 
     def parse(self, response):
+        print(type(response))
+        print(response.url)
+#        print(response.text)
+
+
         bksel = response.xpath("//li[@class='subject-item']")
+        print(type(bksel))
         print("Totally %d items found" %(len(bksel)))
 
-        for it in bksel:
+        if len(bksel) == 0:
+            print("~~~Attention, probably this is the last section!!!")
+            return
 
+        for it in bksel:
             title = it.xpath("div[@class='info']/h2/a/text()").extract()[0].strip()
             if it.xpath("div[@class='info']/h2/a/span/text()").extract_first() != None:
                 title += it.xpath("div[@class='info']/h2/a/span/text()").extract_first()
@@ -57,7 +74,7 @@ class BookSpider(scrapy.Spider):
 
             print("Title:%s  Rating:%s  Pub:%s" %(title, rating, pubinfo))
             bitem = BookItem()
-            bitem["tag"] = 0 #Programing
+            bitem["tag"] = 1
             bitem["title"] = title
             bitem["author"] = pubinfo 
             bitem["rating"] = float(rating)
@@ -66,5 +83,17 @@ class BookSpider(scrapy.Spider):
 
             yield bitem
 
+        print('keep moving...')
+
+        pgsel = response.xpath("//div[@class='paginator']/span[@class='next']/link/@href")
+        print(type(pgsel))
+        print(pgsel)
+        if len(pgsel.extract()) > 0:
+
+            next_url = response.urljoin(pgsel.extract()[0])
+            print("next url is : %s" %(next_url))
+            next_url2 = response.urljoin(next_url)
+            self.global_count = self.global_count + 1
+            yield scrapy.Request(url = next_url)
 
         pass
