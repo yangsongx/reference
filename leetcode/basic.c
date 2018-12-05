f4d5f0362b955864100d94d47b1e71e6450bf86f
Yang Songxiang <yangsonxiang@debian.org>
2018-12-05 10:40:22 +0800

    pass some questions

--- leetcode/basic.c
+++ leetcode/basic.c
@@ -257,17 +257,232 @@ char** findWords(char** words, int wordsSize, int* returnSize) {
 }
 
 //==========================================================================================//
+typedef struct {
+    int digit;
+    struct list_head list;
+}digit_t;
 
+int print_digit(int num, int *len, struct list_head *hdr) {
+    int count = 0;
+    digit_t *d;
+
+    while(num > 0) {
+        printf("%d ", num % 10);
+        d = (digit_t *)malloc(sizeof(digit_t));
+        d->digit = (num % 10);
+        add_like_queue(&d->list, hdr);
+
+        num = num / 10;
+        count++;
+    }
+    printf("\n");
+    *len = count;
+
+    return count;
+}
+
+int do_digit(int num) {
+    struct list_head hdr;
+    hdr.prev = hdr.next = &hdr;
+    struct list_head *pos;
+
+    int return_len = 0;
+    int i = 0;
+
+    while(print_digit(num, &return_len, &hdr) > 1) {
+        i = 0;
+        for(pos = hdr.next; pos != &hdr; pos = pos->next) {
+            digit_t *dg = list_entry(pos, digit_t, list);
+            i += dg->digit;
+        }
+
+        num = i; // do it again!
+        hdr.prev = hdr.next = &hdr;
+    }
+
+    return num;
+}
 int addDigits(int num) {
+    return do_digit(num);
+}
+//==========================================================================================//
+/**
+  * Definition for singly-linked list.
+  * struct ListNode {
+  *     int val;
+  *     struct ListNode *next;
+  * };
+  */
+// note - the struct already at above code!
+struct ListNode* middleNode(struct ListNode* head) {
+    int count = 0;
+    int middle;
+
+    struct ListNode *pos;
+    for(pos = head; pos != NULL; pos = pos->next)
+    {
+        count++;
+    }
+
+    printf("list node totally %d\n", count);
+
+    middle = count/2;
+    printf("middle=%d\n", middle);
+
+    count = 0;
+    for(pos = head; pos != NULL; pos = pos->next)
+    {
+        if(count++ == middle) {
+            break;
+        }
+    }
+
+    return pos;
+        
+}
+
+void test_middleNode()
+{
+    struct ListNode hdr;
+
+    struct ListNode nodes[6];
+    nodes[0].val = 1;
+    hdr.next = &nodes[0];
+
+    nodes[1].val = 2;
+    nodes[0].next = &nodes[1];
+
+    nodes[2].val = 3;
+    nodes[1].next = &nodes[2];
+
+    nodes[3].val = 4;
+    nodes[2].next = &nodes[3];
+
+    nodes[4].val = 5;
+    nodes[3].next = &nodes[4];
+
+    nodes[5].val = 6;
+    nodes[4].next = &nodes[5];
+
+    nodes[5].next = NULL;
+
+    struct ListNode *middle = middleNode(hdr.next);
+
+    struct ListNode *pos;
+    for(pos = middle; pos != NULL; pos = pos->next)
+    {
+        printf("The middle data:%d\n", pos->val);
+    }
+
+}
+//==========================================================================================//
+int numUniqueEmails(char** emails, int emailsSize) {
     return 0;
 }
+//==========================================================================================//
+int hammingDistance(int x, int y) {
+    int b = x ^ y;
+    printf("the value:%d %#x\n", b, b);
+    int count = 0;  
+        while ( b )  
+                {  
+                            if( b& 1 ){  
+                                            ++count;  
+                                                    }  
+                                    b>>= 1;  
+                                        }  
+        printf("now, the 1 has %d\n", count);
+    return b;
+}
+typedef int bool;
+//==========================================================================================//
+bool judgeCircle(char* moves) {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 0;
+
+    int i = 0;
+    while(moves[i] != '\0') {
+        switch(moves[i]) {
+            case 'U':
+                a++;
+                break;
+            case 'D':
+                b++;
+                break;
+            case 'L':
+                c++;
+                break;
+            case 'R':
+                d++;
+                break;
+        }
+        i++;
+    }
+
+    printf("the abcd is %d %d %d %d\n\n", a,b,c,d);
+
+    return ((a == b) && (c == d));
+}
+//==========================================================================================//
+bool checkRecord(char* s) {
+    int i = 0;
+    int prev = 0;
+    int a=0,l=0;
+    bool good = 1;
+
+    while(s[i] != '\0'){
+        printf("prev = %d, si=%d, a=%d, l=%d\n", prev, s[i], a, l);
+        if (a > 1 || l > 1) {
+            good = 0;
+            break;
+        }
+
+        if(s[i] == 'A'){
+            a++;
+        } else if (s[i] == 'L') {
+            printf("now, prev = %d\n", prev);
+                if(prev == 0 || prev == 'L'){
+                    l++;
+                } else {
+                    l++;
+                }
+        } else {
+            ;
+        }
+
+        prev = s[i];
+        i++;
+    }
+
+    return good;
+        
+}
+//==========================================================================================//
+int peakIndexInMountainArray(int* A, int ASize) {
+    int i;
+    int fast_i;
+    for(i = 0; i <ASize; i++) {
+        fast_i = i;
+        fast_i ++;
+
+        if (A[i] > A[fast_i]) {
+            break;
+        }
+    }
+
+    return i;
+}
+//==========================================================================================//
 
 int main(int argc, char **argv)
 {
     int b;
     char *a[] = {"abc", "hello"};
-    findWords(a, 2, &b);
-    printf("the return value = %d\n", b);
+//    findWords(a, 2, &b);
+    int i = checkRecord("PPAPLLL");
+    printf("%d\n", i);
 
     return 0;
 }
---
+++ leetcode/basic.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <stdio.h>
+
+class Solution {
+    public:
+        int numUniqueEmails(vector<string>& emails) {
+            return 0;
+        }
+};
+int main(int argc, char **argv)
+{
+    return 0;
+}
